6af0ab87e0a6c9571ff872e4c1309d3648cec930
Azhar <marmuazhar@gmail.com>
2023-10-29 23:37:20 +0530

    loops and operators

---
+++ dart_basics/loops.dart
@@ -0,0 +1,33 @@
+void main() {
+  //syntax
+  // for(data-intialization;condition check;action(inc/dec)){
+  //   //statements
+  // }
+
+  for (int i = 20; i > 0; i--) {
+    print('For: Hello World');
+  }
+
+  //       i<5;    i++;
+  // i=0   true;
+  // i=1   true;
+  // i=2   true;
+  // i=3   true;
+  // i=4   true;
+  // i=5   false
+
+  // int i = 20;
+  // while (i < 5) {
+  //   print('While: Hello World');
+  //   i++;
+  // }
+
+  // // first condition then(if true) statement
+
+  // int j = 20;
+  // do {
+  //   print('Do-While: Hello World');
+  //   j++;
+  // } while (j < 5);
+  // first statement then check condition
+}
